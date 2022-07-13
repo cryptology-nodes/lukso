e67ef4d9403e03fa22ed2e2de452e2d39fbebd66
cryptology-nodes <107956881+cryptology-nodes@users.noreply.github.com>
2022-07-13 17:16:18 +0300

    Update lukso.sh

--- lukso.sh
+++ lukso.sh
@@ -16,14 +16,24 @@ fi
 
 sleep 1 && curl -s https://raw.githubusercontent.com/cryptology-nodes/main/main/logo.sh |  bash && sleep 2
 
+sudo ufw allow 30303/tcp
+sudo ufw allow 30303/udp
+sudo ufw allow 13000/tcp
+sudo ufw allow 12000/udp
+
 mkdir lukso-l16-testnet
 cd lukso-l16-testnet
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+
 sudo curl https://install.l16.lukso.network | sudo bash
 
 sudo lukso network init --chain l16
 sudo lukso network start
 
-
-echo -e "\e[32m все ноды удалены с сервера !\e[39m" && sleep 10
-echo -e "\e[32mЭто был пранк - нода Lukso работает :) \e[39m"
+echo -e "\e[32m Нода Lukso работает :) \e[39m"
 
